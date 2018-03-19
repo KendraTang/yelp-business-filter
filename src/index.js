2bad3e15566417f987e0fe2b92a0bca150524da4
Kendra Tang <st870522@gmail.com>
2018-03-19 22:07:41 +0800

    Add sass-loader and fix mjs issue by react-app-rewired
    https://github.com/timarney/react-app-rewired/issues/66#issuecomment-330649196

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'semantic-ui-css/semantic.min.css'
-import './App.css'
+import './App.scss'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
